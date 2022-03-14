be407b7f20b51e67788135825ac0743c9b92b30e
ztopcuoglu <zfrtopcuoglu@gmail.com>
2022-03-14 21:30:02 +0300

    sass is added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+import Login from "./components/Login";
+
 function App() {
-  return <div className="App">Empty React App Deneme 12345</div>;
+  return (
+    <div className="App">
+      Empty React App Deneme 12345
+      <Login id="Zafer"></Login>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import "./login.scss"
+const Login = ({ id }) => {
+    return (
+        <div className="container">
+            <div className="top">
+                TOP
+            </div>
+            <div className="bottom">
+                BOTTOM
+            </div>
+        </div>
+    )
+}
+export default Login
